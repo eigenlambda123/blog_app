64067cbe674ca64fe15814029a3a4d92d49607d9
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-04 09:03:46 +0800

    Edit Forms added

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/add/', views.add_post, name='add_post')
+    path('post/<int:pk>/edit', views.edit_post, name='edit_post'),
+    path('post/add/', views.add_post, name='add_post'),
 ]
