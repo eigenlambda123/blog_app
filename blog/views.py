97afe4fe8fdc4dc7bec923e24d6d5be9c28fb5cd
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-04 08:51:49 +0800

    Add post form added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
+from .models import Post
 
-# Create your views here.
+# Get add post view
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)  # Basically get the Post form
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
 
+    return render(request, 'blog/add_post.html', {'form':form})
+
+# Get All Post
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts':posts})
+
+# Get Specific Post
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
